f274af4adf607c022c381cb9d5a915f85219e1d6
Abdelrahman Amin <abdelrahmanadsl@yahoo.com>
2020-11-06 12:12:28 +0200

    adding Generating grid

--- src/MissionImpossible.java
+++ src/MissionImpossible.java
@@ -1,4 +1,70 @@
+import java.util.*;
 
 public class MissionImpossible extends SearchProblem {
+	public MissionImpossible() {
 
+	}
+
+	public static String genGrid() {
+		String outputGrid = "";
+		// Axis initialization between 5 and 15 were x is the vertical and y is the
+		// horizontal
+		// and adding the axes to the output string as m,n indexed (0,2)
+		int xAxis = (int) (Math.random() * 11) + 5;
+		int yAxis = (int) (Math.random() * 11) + 5;
+		outputGrid += xAxis + "," + yAxis + ";";
+
+		// Adding Ethan's location ranging between the x and y axes
+		// to the output string as ex,ey indexed (4,6)
+		int ethanXlocation = (int) (Math.random() * xAxis);
+		int ethanYlocation = (int) (Math.random() * yAxis);
+		outputGrid += ethanXlocation + "," + ethanYlocation + ";";
+
+		// Adding the submarine's location ranging between the x and y axes excluding
+		// Ethan's location
+		// to the output string as sx,sy indexed (4,6)
+		while (true) {
+			int submarineXlocation = (int) (Math.random() * xAxis);
+			int submarineYlocation = (int) (Math.random() * yAxis);
+			if ((submarineXlocation != ethanXlocation) || (submarineYlocation != ethanYlocation)) {
+				outputGrid += submarineXlocation + "," + submarineYlocation + ";";
+				break;
+			}
+		}
+		
+
+		// Initialize members number between 5 and 10
+		int imfMembers = (int) (Math.random() * 6) + 5;
+		// Initialize health for each member
+		String health = "";
+		// Generate positions of IMFmembers randomly
+		for (int i = 0; i < imfMembers; i++) {
+			while (true) {
+				// Check if the new position of the member is empty before adding it
+				int newMemberXlocation = (int) (Math.random() * (xAxis + 1));
+				int newMemberYlocation = (int) (Math.random() * (yAxis + 1));
+				String checkMemberPosition = newMemberXlocation + "," + newMemberYlocation;
+				if (!outputGrid.contains(checkMemberPosition)) {
+					outputGrid += newMemberXlocation + "," + newMemberYlocation;
+					health += (int) (Math.random() * 9) + 1;
+					if (i == imfMembers - 1) {
+						health += ";";
+					} else {
+						outputGrid += ",";
+						health += ",";
+					}
+					break;
+				}
+			}
+		}
+		outputGrid += ";" + health;
+		
+		int truckCarry = (int) (Math.random() * (imfMembers + 1));
+		outputGrid += truckCarry;
+		return outputGrid;
+	}
+//	public static void main(String[]ars) {
+//		String test = genGrid();
+//		System.out.println(test);
+//	}
 }
